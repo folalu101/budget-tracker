1a3c61c64fe46887ba3bb2615622fb2bcd8f57a3
A.Adefolalu B <adefolalucode101@gmail.com>
2023-02-01 22:47:27 +0100

    clean up html code and copy html code inside javascript function in app.js

--- src/app.js
+++ src/app.js
@@ -5,14 +5,64 @@ export default class BudgetTracker{
 
         this.root.querySelector(".new-entry").addEventListener("click", ()=>{
             this.onNewEntryBtnClick();
-        })
+        });
+        // load initial data from local storage
+        this.load();
     }
     static html() {
-
+        return `
+        <table class="budget-tracker">
+            <thead>
+                <tr>
+                    <th>Date</th>
+                    <th>Description</th>
+                    <th>Type</th>
+                    <th>Amount</th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody class="entries">
+            </tbody>
+            <tbody>
+                <tr>
+                    <td colspan="5" class="controls">
+                        <button type="button" class="new-entry">New Entry</button>
+                    </td>
+                </tr>
+            </tbody>
+            <tfoot>
+                <tr>
+                    <td colspan="5" class="summary">
+                        <strong>Total:</strong>
+                        <span class="total">$0.00</span>
+                    </td>
+                </tr>
+            </tfoot>
+        </table>`
     }
 
     static entryHtml(){
-
+        return `
+        <tr>
+                <td>
+                    <input class="input input-date" type="date">
+                    </td>
+                    <td>
+                        <input class="input input-escribtion" type="text" placeholder="Add Description (e.g Wages, bills, etc)">
+                    </td>
+                    <td>
+                        <select class="input input-type">
+                            <option value="income">Income</option>
+                            <option value="expense">Expense</option>
+                        </select>
+                    </td>
+                    <td>
+                        <input type="number" class="input input-amount">
+                    </td>
+                    <td>
+                        <button type="button" class="delete-entry">&#10005;</button>
+                    </td>
+                </tr>`
     }
 
     load(){
